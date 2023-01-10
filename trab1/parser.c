1c9c2b8dcf5d2ed5868a4cde4ba50515a9646ebf
EugenioAL <32808464+EugenioAL@users.noreply.github.com>
2023-01-10 11:46:44 -0400

    parser 1.0

--- trab1/parser.c
+++ trab1/parser.c
@@ -3,3 +3,19 @@
 void helloP(){
     printf("Hello from the parser\n");
 }
+
+void prox_char(char *nome){
+    FILE* arq;
+    char c = 'E';
+    arq = fopen(nome,"r");
+
+    printf("%s\n",nome);
+    while ((c = getc(arq)) != EOF)
+    {
+        if(c != '\n' && c != '\t' && c != ' '){
+            printf("%c \n",c);
+        }
+    }
+            printf("\n");
+
+}
