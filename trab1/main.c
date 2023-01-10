77e5bfe9885276edeba4c77d38def9e2a1e9616c
EugenioAL <32808464+EugenioAL@users.noreply.github.com>
2023-01-10 11:48:51 -0400

    parser1.0

--- trab1/main.c
+++ trab1/main.c
@@ -1,10 +1,276 @@
-#include <stdio.h>
-#include <string.h>
 #include "myComp.h"
 
-int main(){
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*#include "myComp.h"
+
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*
+    if(arq != NULL){
+        while(fgets(str,100,arq)){
+            printf("%s",str);
+        }
+    }
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
     printf("hello from the main\n");
   
     helloP();
-    return 1;
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
+}
+
+    if(arq != NULL){
+        while(fgets(str,100,arq)){#include "myComp.h"
+
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*
+    if(arq != NULL){
+        while(fgets(str,100,arq)){
+            printf("%s",str);
+        }
+    }
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
+    printf("hello from the main\n");#include "myComp.h"
+
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*
+    if(arq != NULL){
+        while(fgets(str,100,arq)){
+            printf("%s",str);
+        }
+    }
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
+    printf("hello from the main\n");
+  
+    helloP();
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
+}
+
+  
+    helloP();
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
+}
+
+            printf("%s",str);
+        }
+    }#include "myComp.h"
+
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*
+    if(arq != NULL){
+        while(fgets(str,100,arq)){
+            printf("%s",str);
+        }
+    }
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
+    printf("hello from the main\n");
+  
+    helloP();
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
+}
+
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
+    printf("hello from the main\n");
+  
+    helloP();#include "myComp.h"
+
+int main(int argc, char *argv[]){
+
+    unsigned int b;
+    int a;
+    char str[200];
+    FILE* arq;
+
+    arq = fopen(argv[1],"r");
+
+    /*
+    if(arq != NULL){
+        while(fgets(str,100,arq)){
+            printf("%s",str);
+        }
+    }
+    else{
+        printf("Erro\n");
+    }
+    fclose(arq);*/
+
+    prox_char(argv[1]);
+    printf("hello from the main\n");
+  
+    helloP();
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
+}
+
+
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("1");
+        break;
+    case 2:
+        printf("2");
+        break;
+    case 3:
+        printf("3");
+        break;
+    
+    default:
+        break;
+    }
+
+    return a;
 }
